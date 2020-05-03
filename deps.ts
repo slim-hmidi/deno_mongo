beed4df10644e28f4be1075279a54488ca412417
Slim Hmidi <31925861+slim-hmidi@users.noreply.github.com>
2020-05-04 01:00:01 +0200

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { prepare } from "https://raw.githubusercontent.com/slim-hmidi/deno-plugin-prepare/master/mod.ts";
+export { prepare } from "https://raw.githubusercontent.com/slim-hmidi/deno-plugin-prepare/dev%400.3.1/mod.ts";
